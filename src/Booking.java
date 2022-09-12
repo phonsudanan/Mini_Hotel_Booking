5e132e2dabdcfcfef97ed28c68e608f9fed0ed88
phonsudanan <phonsuda-nan@hotmail.com>
2022-09-12 16:11:08 +0700

    second commit

--- src/Booking.java
+++ src/Booking.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.sql.Connection;
 import java.sql.ResultSet;
 
@@ -17,13 +19,34 @@ public class Booking extends JFrame {
         setLocationRelativeTo(null);
         setContentPane(home);
 //        BookingRoom(001);
+        CANCELButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                int result = JOptionPane.showConfirmDialog(null,"ออกจากหน้าจอการจอง ?","Close",JOptionPane.YES_NO_OPTION);
+                if(result == JOptionPane.YES_OPTION){
+                    setVisible(false);
+                }
+
+            }
+        });
+        CONFIRMButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                int result = JOptionPane.showConfirmDialog(null,"ยืนยันการจองห้องพัก ?","ยืนยันการจอง",JOptionPane.YES_NO_OPTION);
+                if(result == JOptionPane.YES_OPTION){
+
+                    JOptionPane.showMessageDialog(null,"การจองสำเร็จ");
+                    setVisible(false);
+                }
+            }
+        });
     }
 
-    void BookingRoom(int room_number){
+    void BookingRoom(String room_id){
 try {
     CalendarSearch c = new CalendarSearch();
 //    c.panelIn.
-    String sql = "SELECT * FROM room where room_id = " + room_number;
+    String sql = "SELECT * FROM room where room_id = " + room_id;
     ResultSet rs = conn.createStatement().executeQuery(sql);
     while (rs.next()) {
         room.setText(rs.getString("room_number"));
@@ -42,7 +65,7 @@ try {
     private JPanel home;
     private JButton CANCELButton;
     private JButton CONFIRMButton;
-    private JTextField textField1;
+    private JTextField customer;
     private JLabel area;
     private JLabel bed;
     private JLabel price;
--- src/HomePage.java
+++ src/HomePage.java
@@ -2,13 +2,19 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.text.ParseException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 
 public class HomePage extends JFrame {
     Booking b = new Booking();
     CalendarDateInDateOut c = new CalendarDateInDateOut();
     CalendarSearch s = new CalendarSearch();
-
+    String in,out;
+    long daysBetween;
+    static String room_id;
     public static void main(String[] args) throws ParseException {
         UIManager.put("OptionPane.messageFont", new Font("Leelawadee", Font.PLAIN, 12));
         UIManager.put("InternalFrame.titleFont", new Font("Leelawadee", Font.PLAIN, 12));
@@ -36,33 +42,109 @@ public class HomePage extends JFrame {
                 buttonSearch.addMouseListener(new MouseAdapter() {
                     @Override
                     public void mouseClicked(MouseEvent e) {
-                        String in = c.pattern.format(s.jdIn.getDate());
-                        String out = c.pattern.format(s.jdOut.getDate());
+                        buttonSearch.setSelected(true);
+                         in = c.pattern.format(s.jdIn.getDate());
+                         out = c.pattern.format(s.jdOut.getDate());
+                        DateTimeFormatter df = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+                        LocalDate date1 = LocalDate.parse(in, df);
+                        LocalDate date2 = LocalDate.parse(out, df);
+                        daysBetween = ChronoUnit.DAYS.between(date1, date2);
                         System.out.println(in +"\n"+ out);
-                    }
-                });
 
-                A01.addMouseListener(new MouseAdapter() {
-                    @Override
-                    public void mouseClicked(MouseEvent e) {
-                        int n = 001;
-                        b.in.setText(s.in);
-                        b.out.setText(s.out);
-                        b.day.setText(String.valueOf(s.daysBetween) + "\t วัน");
-                        b.BookingRoom(n);
-                        b.setVisible(true);
                     }
                 });
-
-                buttonBooking.addMouseListener(new MouseAdapter() {
-                    @Override
-                    public void mouseClicked(MouseEvent e) {
-                        s.setVisible(true);
-                    }
-                });
+        buttonBooking.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                s.setVisible(true);
             }
+        });
+
+        A01.addMouseListener(click(A01));
+        A02.addMouseListener(click(A02));
+        A03.addMouseListener(click(A03));
+        A04.addMouseListener(click(A04));
+        B01.addMouseListener(click(B01));
+        B02.addMouseListener(click(B02));
+        B03.addMouseListener(click(B03));
+        B04.addMouseListener(click(B04));
+        C01.addMouseListener(click(C01));
+        C02.addMouseListener(click(C02));
+        C03.addMouseListener(click(C03));
+        C04.addMouseListener(click(C04));
+        D01.addMouseListener(click(D01));
+        D02.addMouseListener(click(D02));
+        D03.addMouseListener(click(D03));
+        D04.addMouseListener(click(D04));
+    }
 
 
+    MouseListener click(JButton buttonR){
+        buttonR.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                String click = buttonR.getActionCommand();
+                if(click.equals("A01")){
+                    room_id = "001";
+                } else if (click.equals("A02")){
+                    room_id = "002";
+                }else if (click.equals("A03")){
+                    room_id = "003";
+                }else if (click.equals("A04")){
+                    room_id = "004";
+                }else if (click.equals("B01")){
+                    room_id = "005";
+                }else if (click.equals("B02")){
+                    room_id = "006";
+                }else if (click.equals("B03")){
+                    room_id = "007";
+                }else if (click.equals("B04")){
+                    room_id = "008";
+                }else if (click.equals("C01")){
+                    room_id = "009";
+                }else if (click.equals("C02")){
+                    room_id = "010";
+                }else if (click.equals("C03")){
+                    room_id = "011";
+                }else if (click.equals("C04")){
+                    room_id = "012";
+                }else if (click.equals("D01")){
+                    room_id = "013";
+                }else if (click.equals("D02")){
+                    room_id = "014";
+                }else if (click.equals("D03")){
+                    room_id = "015";
+                }else if (click.equals("D04")){
+                    room_id = "016";
+                } else {
+                    room_id = "0";
+                }
+                if( buttonSearch.isSelected()){
+                    MSearch();
+                }else {
+                    CSearch();
+                }
+            }
+        });
+        return null;
+    }
+
+
+void MSearch(){
+    b.in.setText(in);
+    b.out.setText(out);
+    b.day.setText(String.valueOf(daysBetween) + "\t วัน");
+    b.BookingRoom(room_id);
+    b.setVisible(true);
+}
+
+void CSearch(){
+    b.in.setText(s.in);
+    b.out.setText(s.out);
+    b.day.setText(String.valueOf(s.daysBetween) + "\t วัน");
+    b.BookingRoom(room_id);
+    b.setVisible(true);
+}
 
 
 
@@ -94,4 +176,5 @@ public class HomePage extends JFrame {
     private JButton D02;
     private JButton D04;
     private JButton D03;
+    private JButton C02;
 }
